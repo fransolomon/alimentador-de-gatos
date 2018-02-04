fab163cb71832192a56a324856a9fd8edcb2e5c0
fransolomon <pgfrancisco@gmail.com>
2018-02-04 20:05:05 +0100

    nuevo firmaware
    Mejorando los tiempos, tengo que prepararlo todo en plan funcion para que sea mas facilmente usable el codigo

--- comederov1.ino
+++ comederov1.ino
@@ -3,69 +3,19 @@
 
 
 //  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-
-/*************************************************************
-
-  Download latest Blynk library here:
-
-    https://github.com/blynkkk/blynk-library/releases/latest
-
-
-
-  Blynk is a platform with iOS and Android apps to control
-
-  Arduino, Raspberry Pi and the likes over the Internet.
-
-  You can easily build graphic interfaces for all your
-
-  projects by simply dragging and dropping widgets.
-
-
-
-    Downloads, docs, tutorials: http://www.blynk.cc
-
-    Sketch generator:           http://examples.blynk.cc
-
-    Blynk community:            http://community.blynk.cc
-
-    Follow us:                  http://www.fb.com/blynkapp
-
-                                http://twitter.com/blynk_app
-
-
-
-  Blynk library is licensed under MIT license
-
-  This example code is in public domain.
-
-
-
- *************************************************************
-
-
-
-  Rotate a servo using a slider!
-
-
-
-  App project setup:
-
-    Slider widget (0...180) on V3
-
- *************************************************************/
-
-
-
-/* Comment this out to disable prints and save space */
-
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <Servo.h>
 #define SERVO_PIN 0
 #define SERVO_COMER 5
-#define SERVO_STOP 5
-#define TIEMPO_COMIDA 5
+#define SERVO_STOP 83
+#define SERVO_REVERSA 180
+#define TIEMPO_COMIDA 10000
+//#define TIEMPO_REVERSA 500
+//#define TIEMPO_FOWARD 1500
+#define TIEMPO_REVERSA 250
+#define TIEMPO_FOWARD 750
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
 
@@ -73,19 +23,26 @@ char auth[] = "4e40c05ed07b40558c5daafb574f8fe7";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "softronica_01";
-char pass[] = "el_perro_de_san_roque";
+//char ssid[] = "softronica_01";
+//char pass[] = "el_perro_de_san_roque";
+char ssid[] = "Guaynet";
+char pass[] = "raqueleslamejor";
 int dardecomer=0;
 unsigned long timer_millis_1_inicio=0;
 unsigned long timer_millis_1_fin=0;
 unsigned long timer_millis_1_curent=0;
+unsigned long timer_real =0;
+unsigned long resta_timer = 0;
+
+unsigned long intervalo_motor = 0;
+int rotacion=0;
 
 Servo servo;
 BLYNK_WRITE(V3)
 {
   dardecomer=param.asInt();
   timer_millis_1_inicio=millis()+TIEMPO_COMIDA;
-  
+  resta_timer = millis();
 }
 
 BLYNK_APP_CONNECTED() {
@@ -116,11 +73,13 @@ void loop()
   if(dardecomer==1)
   {
    servo.attach(SERVO_PIN);
-   servo.write(SERVO_COMER);
-   Serial.print("COMER");
+   alimentar();
+   
    if (timer_millis_1_curent>=timer_millis_1_inicio)
     {
     dardecomer=0;
+    intervalo_motor=0;
+    rotacion=0;
     }
   }
 
@@ -130,7 +89,72 @@ void loop()
   delay(500);
   Serial.print("NOCOMER");
   }  
+}
 
- 
+
+
+void alimentar()
+{
+timer_real = timer_millis_1_curent-resta_timer;
+
+if (timer_real>=intervalo_motor)
+  {
+  if(rotacion == 1)
+    {
+    intervalo_motor = intervalo_motor + TIEMPO_FOWARD; 
+    servo.write(SERVO_COMER); 
+    rotacion = 0;
+    Serial.println (" SERVO ADELANTE ");
+    }
+    else if (rotacion == 0)
+    {
+    servo.write(SERVO_REVERSA);
+    Serial.println (" SERVO ATRAS ");
+    intervalo_motor = intervalo_motor + TIEMPO_REVERSA;  
+    rotacion = 1;
+    }
+  }
+  // AQUI EN ESTE PUNTO PODEMOS INTRODUCIR EL TEMA DE LOS ATASCOS EN EL MOTOR Y SU ANALISIS.
+  Serial.println("");
+  Serial.print("Timer real: ");
+  Serial.print(timer_real);
+  Serial.print(" proximo cambio: ");
+  Serial.print(intervalo_motor);
+  if (rotacion==0)
+  {
+    Serial.print (" SERVO ADELANTE ");
+  }else{
+    Serial.print (" SERVO ATRAS ");
+  }  
+  Serial.print(" rotacion: ");
+  Serial.println(rotacion);
+  Serial.println("----------------------");
+}
+
+void atranco_alimentar()
+{
+timer_real = timer_millis_1_curent-resta_timer;
+
+if (timer_real>=intervalo_motor)
+  {
+  if(rotacion == 1)
+    {
+    intervalo_motor = intervalo_motor + 150; 
+    servo.write(SERVO_COMER); 
+    rotacion = 0;
+    Serial.println (" SERVO ADELANTE ");
+    }
+    else if (rotacion == 0)
+    {
+    servo.write(SERVO_REVERSA);
+    Serial.println (" SERVO ATRAS ");
+    intervalo_motor = intervalo_motor + 150;  
+    rotacion = 1;
+    }
+  }
 
 }
+
+  
+
+
